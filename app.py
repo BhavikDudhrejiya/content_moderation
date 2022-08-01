c721ef21fecf12de647309d3cb81388d35e98096
Bhavik Dudhrejiya <77838416+BhavikDudhrejiya@users.noreply.github.com>
2022-08-01 23:19:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,21 +8,37 @@ from Utils import generate_moderation_data_using_image_path, generate_moderation
 import warnings
 from Utils import flags_map, safe_flag
 warnings.filterwarnings('ignore')
+#------------------------------------------------------------------------------------------------------------------------------
 
 #Adding Content moderation Title
 st.sidebar.title('MODERATION APPLICATION')
 
 #Adding header
-st.header('''CONTENT MODERATION USING CLIP MODEL''')
+st.header('''IMAGE MODERATION''')
 
 #Selecting url or image
-image_source_type = st.sidebar.radio('Please select below:',['Single JPG Image',
-                                                             'Single URL Image', 
-                                                             'Multiple JPG Images',
-                                                             'Multiple URL Images',
-                                                             'Single JPG Image with two context'])
+image_source_type = st.sidebar.radio('Please select below:',['About',
+                                                             'Single JPG Image',
+                                                             'Single URL Image',
+                                                             'Single URL Image with two context'])
 ##############################################################################################################
-if image_source_type=='Single JPG Image':
+if image_source_type=='About':
+
+    st.write('This application can detect explicit and graphical content in the image.')
+    
+    st.write('\n')
+
+    st.subheader('Moderation Flags:')
+    st.write('`1. EXPLICIT NUDITY & SUGGESTIVE`')
+    st.write('`2. ALCOHOL`')
+    st.write('`3. DRUGS & TOBACCO`')
+    st.write('`4. VIOLENCE & VISUALLY DISTURBING`')
+    st.write('`5. RUDE GESTURES & HATE SYMBOLS`')
+    st.write('`6. GAMBLING`')
+    st.write('`7. NEUTRAL`')
+
+##############################################################################################################
+elif image_source_type=='Single JPG Image':
     
     #Taking image input through upload
     image_input = st.file_uploader('Moderation on single JPG image:', type=['jpg'])
@@ -61,48 +77,6 @@ elif image_source_type=='Single URL Image':
         st.metric(label='Moderation Label:', value=labels)
         st.metric(label='Moderation Probability:', value=f'{round(probs*100,2)}%')
 
-##############################################################################################################
-elif image_source_type=='Multiple JPG Images':
-    #Taking multiple image through image path
-    image_input = st.text_input('Moderation on multiple JPG image:')
-
-    threshold = st.slider('Probability threshold:',0.51,0.99,0.1)
-
-    #Generate moderation data
-    try:
-        moderation_data = generate_moderation_data_using_image_path(image_input, threshold)
-        df = moderation_data.to_csv()
-    except:
-        st.markdown("""###### Please provide image folder path & click below""")
-
-    #Generate content moderation data and prfloat on screen
-    if st.button('GENERATE MODERATION DATA'):
-        st.dataframe(moderation_data.head(10))
-        #Download the data
-        st.download_button('DOWNLOAD',data=df,file_name='Moderation_Data.csv',)
-##############################################################################################################
-elif image_source_type=='Multiple URL Images':
-    #Taking multiple image through image url csv file
-    image_input = st.file_uploader('Moderation on multiple URL image:', type=['csv'])
-    if image_input is not None:
-        image_data = pd.read_csv(image_input)
-
-    threshold = st.slider('Probability threshold:',0.51,0.99,0.5)
-
-    #Generate moderation data
-    try:
-        moderation_data_url = generate_moderation_data_using_image_url(image_data['Images_ID'], threshold)
-        df = moderation_data_url.to_csv()
-    except:
-        st.markdown("""###### Please provide multiple images url csv file & click below""")
-
-    # #Generate content moderation data and prfloat on screen
-    if st.button('GENERATE MODERATION DATA'):
-        st.write(f'Data Shape: {image_data.shape}')
-        st.dataframe(moderation_data_url.head())
-    
-        #Download the data
-        st.download_button('DOWNLOAD',data=df,file_name='Moderation_Data.csv')
 ##############################################################################################################
 else:
     #Taking image input through url
